3b809486e53554b86ce3260d3fa9e290880a831d
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-04-26 21:15:10 +0400

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
 
 
 app = Flask(__name__)
@@ -16,6 +16,10 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_appcontext(response_or_exc):
     storage.close()
+
+@app.errorhandler(404)
+def error_handler(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
